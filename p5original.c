f06f6768999506b24741696545d02c8e9efb10b9
pavannagur123 <94945161+pavannagur123@users.noreply.github.com>
2021-12-15 03:00:59 +0000

    string

--- p5original.c
+++ p5original.c
@@ -16,10 +16,10 @@ int main()
 int largest(int a ,int b,int c)
 {
   
-  if(a>b && a>c)
+  if(a>=b && a>=c)
   return a;
 
-  if (b>c && b>a)
+  if (b>=c && b>=a)
   return b;
   else
   return c;
--- p9original.c
+++ p9original.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+#define SIZE 
+char a[SIZE], b[SIZE];
+void input(){
+    printf("Enter string1: ");
+    scanf("%s", &a);
+    
+    scanf("%s", &b);
+}
+int stringcmp(){
+    int i = 0;
+    while(a[i]!='\0' || b[i] != '\0')
+    {
+        if(a[i] != b[i]){
+            if(a[i] > b[i]) return 1;
+            else return 2;
+        }
+        i++;
+    }
+}
+void output(int n){
+    if(n == 1) printf("String %s is greater..\n", a);
+    else printf("String %s is greater..\n", b);
+}
+
+int main(){
+    int val;
+    input();
+    val = stringcmp();
+    if(val == 1) output(1);
+    else output(2);
+
+    return 0;
+}
+
+
+
  
