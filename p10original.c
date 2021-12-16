abb2bc5918ebf829a3150fb6c2aa38b4421d29bf
pavannagur123 <94945161+pavannagur123@users.noreply.github.com>
2021-12-16 03:22:21 +0000

    complex

--- p10original.c
+++ p10original.c
@@ -1,2 +1,29 @@
+#include <stdio.h>
+void input(int *a,int *b ,int *c,int *d);
+void x(int *a,int *b ,int *c,int *d,int *imag,int *real);
+void output(int *imag,int *real);
+int main()
+{
+    int a,b,c,d,imag,real;
+    input (&a,&b,&c,&d);
+    x (&a,&b,&c,&d,&imag,&real);
+    output(&imag,&real);
+    return 0;
+}
+void input(int *a,int *b ,int *c,int *d)
+{
+  scanf("%d %d",a,b);
+  scanf("%d %d",c,d);
+}
+void x(int *a,int *b ,int *c,int *d,int *imag,int *real)
+{
+  *imag=*b+*d;
+  *real=*a+*c;
+
+} 
+void output(int *imag,int *real)
+{
+  printf("the sum is %d + %d i  ",*real,*imag);
+}
 
 
